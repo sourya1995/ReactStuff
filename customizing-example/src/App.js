9351e9ce28f05a8a3f238fbf67d6c6de696283b9
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-14 12:20:52 +0530

    Adding multiple components

--- customizing-example/src/App.js
+++ customizing-example/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Heading from './components/Heading';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <Heading firstName="Bob"/>
-      <Heading firstName='Nick'/>
+      <h1>Task: Add three card elements</h1>
+      <Card h2 = "First Card's h2" h3 = "First card's h3"/>
+      <Card h2 = "Second Card's h2" h3 = "Second card's h3"/>
+      <Card h2 = "Third Card's h2" h3 = "Third card's h3"/>
     </div>
+    
   );
 }
 
