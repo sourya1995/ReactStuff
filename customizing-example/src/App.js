b17055cd388396a9c102cebc6f0ab4cac0741016
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-14 13:07:09 +0530

    adding basic event handling for a button click

--- customizing-example/src/App.js
+++ customizing-example/src/App.js
@@ -4,12 +4,17 @@ import Heading from './components/Heading';
 import Card from './components/Card';
 
 function App() {
+
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt("type a number");
+    alert(`Computer Number: ${randomNum}, Your guess ${userInput}`);
+  }
   return (
     <div className="App">
-      <h1>Task: Add three card elements</h1>
-      <Card h2 = "First Card's h2" h3 = "First card's h3"/>
-      <Card h2 = "Second Card's h2" h3 = "Second card's h3"/>
-      <Card h2 = "Third Card's h2" h3 = "Third card's h3"/>
+      <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>Guess a number between 1 and 3</button>
     </div>
     
   );
