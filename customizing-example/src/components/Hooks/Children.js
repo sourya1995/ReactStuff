b16d0e4eb7fdef9c739a7ce22438a6e6983b6791
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-19 10:08:05 +0530

    Adding children prop

---
+++ customizing-example/src/components/Hooks/Children.js
@@ -0,0 +1,46 @@
+import * as React from "react";
+
+export const RadioGroup = ({ onChange, selected, children }) => { 
+ const RadioOptions = React.Children.map(children, (child) => { 
+   return React.cloneElement(child, { 
+     onChange, 
+     checked: child.props.value === selected, 
+   }); 
+ }); 
+ return <div className="RadioGroup">{RadioOptions}</div>; 
+}; 
+ 
+export const RadioOption = ({ value, checked, onChange, children }) => { 
+ return ( 
+   <div className="RadioOption"> 
+     <input 
+       id={value} 
+       type="radio" 
+       name={value} 
+       value={value} 
+       checked={checked} 
+       onChange={(e) => { 
+         onChange(e.target.value); 
+       }} 
+     /> 
+     <label htmlFor={value}>{children}</label> 
+   </div> 
+ ); 
+}; 
+
+
+function App() {
+    const [selected, setSelected] = useState("");
+    return (
+      <div className="App">
+        <h2>How did you hear about Little Lemon?</h2>
+        <RadioGroup onChange={setSelected} selected={selected}>
+          <RadioOption value="social_media">Social Media</RadioOption>
+          <RadioOption value="friends">Friends</RadioOption>
+          <RadioOption value="advertising">Advertising</RadioOption>
+          <RadioOption value="other">Other</RadioOption>
+        </RadioGroup>
+        <button disabled={!selected}>Submit</button>
+      </div>
+    );
+  }
