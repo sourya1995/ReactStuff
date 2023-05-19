3d6e7d7a18f9fc91f390a2cbc59b7cc3dfa9da72
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-19 11:59:52 +0530

    adding unit tests

--- customizing-example/src/App.test.js
+++ customizing-example/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -6,3 +6,47 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe("Feedback Form", () => {
+  test('user is able to submit the form if the score is lower than 5 and additional feedback is provided', () => {
+    const score = 3;
+    const comment = "The pizza crust was too thick";
+    const handleSubmit = jest.fn();
+    render(<FeedbackForm onSubmit={handleSubmit} />);
+
+    const rangeInput = screen.getByLabelText(/Score:/);
+    fireEvent.change(rangeInput, { target: { value: score } });
+
+    const textArea = screen.getByLabelText(/Comments :/);
+    fireEvent.change(textArea, { target: { value: comment } });
+
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+
+    expect(handleSubmit).toHaveBeenCalledWith({
+      score,
+      comment,
+    });
+
+  });
+
+  test('user is able to submit the form if the score is higher than 5 and additional feedback is not provided', () => {
+    const score = 9;
+    const handleSubmit = jest.fn();
+    render(<FeedbackForm onSubmit={handleSubmit} />);
+
+    const rangeInput = screen.getByLabelText(/Score:/);
+    fireEvent.change(rangeInput, { target: { value: score } });
+
+
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+
+    expect(handleSubmit).toHaveBeenCalledWith({
+      score,
+      comment: ""
+    });
+  });
+});
+
+
