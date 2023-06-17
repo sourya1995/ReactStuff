1e5c1edea52440483793e9271cc4a5526569f5e7
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-17 15:25:02 +0530

    specifying rendering order

--- tailwind/layout.js
+++ tailwind/layout.js
@@ -16,4 +16,12 @@
         <div class="text-center w-1/5">Right Sidebar</div>
         <div class="w-full">Footer</div>
     </div>
+
+    <div class="flex flex-row flex-wrap w-1/3">
+        <div class="text-center w-3/5 order-3">Content</div>
+        <div class="w-full order-1">Header</div>
+        <div class="text-center w-1/5 order-2">Left Sidebar</div>
+        <div class="text-center w-1/5 order-4">Right Sidebar</div>
+        <div class="w-full order-5">Footer</div>
+    </div>
 </>
