4f5bc207dbe6830e158d481c417c78d7957b9bc2
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-17 15:20:00 +0530

    sample layout

---
+++ tailwind/layout.js
@@ -0,0 +1,19 @@
+<>
+    <div class="flex flex-col w-1/3">
+        <div class="flex-grow">Header</div>
+        <div class="flex flex-row"> //one full row for content
+            <div class="text-center w-1/5">Left Sidebar</div>
+            <div class="text-center w-3/5">Content</div>
+            <div class="text-center w-1/5">Right Sidebar</div>
+        </div>
+        <div class="flex-grow">Footer</div>
+    </div>
+
+    <div class="flex flex-row flex-wrap w-1/3"> //everything is a row
+        <div class="w-full">Header</div>
+        <div class="text-center w-1/5">Left Sidebar</div>
+        <div class="text-center w-3/5">Content</div>
+        <div class="text-center w-1/5">Right Sidebar</div>
+        <div class="w-full">Footer</div>
+    </div>
+</>
