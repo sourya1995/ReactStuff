48ceabea4d163eedb0d779438ecbe623c99ed2da
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-08-26 13:26:41 +0530

    adding a reusable component using children

--- 01-starting-setup/src/Components/ExpenseItem.js
+++ 01-starting-setup/src/Components/ExpenseItem.js
@@ -1,19 +1,21 @@
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props){
    
    
     return (
-       <div>
-            <div className="expense-item">
+       
+            <Card className="expense-item">
                <ExpenseDate date={props.date}/>
-            </div>
+            
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-       </div>
+            </Card>
+       
     );
 }
 
