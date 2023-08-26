cda95ca17c898400c78dc1673d54567de65288b3
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-08-26 17:25:40 +0530

    adding expense form and basic state

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./Components/Expenses";
+import NewExpense from "./Components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -24,7 +25,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
--- 01-starting-setup/src/Components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/Components/Expenses/ExpenseItem.js
@@ -1,10 +1,20 @@
+import React, {useState} from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props){
+
    
+   const [title, setTitle] = useState(props.title);
    
+   const clickHandler = () => {
+        setTitle('Updated');
+        console.log(title);
+   }
+   
+    
     return (
        
             <Card className="expense-item">
@@ -14,6 +24,7 @@ function ExpenseItem(props){
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
             </Card>
        
     );
