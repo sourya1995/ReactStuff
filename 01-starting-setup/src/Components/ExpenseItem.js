03ac49770b6176a66149612296b7258fa933865a
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-08-26 11:18:50 +0530

    dynamic data passing

--- 01-starting-setup/src/Components/ExpenseItem.js
+++ 01-starting-setup/src/Components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(){
+
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
     return (
        <div>
-            <div className="expense-item">Date</div>
+            <div className="expense-item">{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
        </div>
     );
