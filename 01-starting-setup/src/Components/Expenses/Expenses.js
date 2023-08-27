a38fe2408725f5acd2175efc2fec2a62a9806892
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-08-27 10:52:36 +0530

    Two Way binding

--- 01-starting-setup/src/Components/Expenses/Expenses.js
+++ 01-starting-setup/src/Components/Expenses/Expenses.js
@@ -1,13 +1,24 @@
+import ExpensesFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import Card from "./UI/Card";
+import React, {useState} from "react";
 
-function Expenses(props) {
+const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
     return (
-        <div className="expenses">
+        <div>
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+        
+        <Card className="expenses">
             <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
             <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
             <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
             <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
+        </Card>
         </div>
     )
 }
