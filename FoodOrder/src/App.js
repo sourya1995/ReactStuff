6a6e94722cc15ba214a8a9030933269aebe31c3f
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-08-30 12:03:30 +0530

    adding a modal

--- FoodOrder/src/App.js
+++ FoodOrder/src/App.js
@@ -1,9 +1,11 @@
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import { Fragment } from 'react';
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
--- FoodOrder/src/components/Cart/Cart.js
+++ FoodOrder/src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import classes from 'Cart.module.css';
+import Modal from '../UI/Modal';
 
 const Cart = props => {
     const cartItems = <ul className={classes['cart-items']}>{[{
@@ -7,7 +8,7 @@ const Cart = props => {
         amount: 2,
         price: 12.99
     }].map(item => <li>{item.name}</li>)}</ul>
-    return <div>
+    return <Modal>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
@@ -17,7 +18,7 @@ const Cart = props => {
             <button className={classes['button--alt']}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
-    </div>
+    </Modal>
 };
 
 export default Cart;
