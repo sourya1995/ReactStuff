5cff905a33c21a68664ce9275c69fde19ac7f7c7
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-01 11:24:30 +0530

    adding Error Boundary

---
+++ ClassBasedComponents/src/components/ErrorBoundary.js
@@ -0,0 +1,21 @@
+class ErrorBoundary extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {hasError: false}
+    }
+
+    componentDidCatch(error){
+        this.setState({hasError: true});
+    }
+
+    render(){
+        if(this.state.hasError){
+            return <p>Something went wrong!</p>
+        }
+        return this.props.children;
+    }
+}
+
+
+export default ErrorBoundary;
