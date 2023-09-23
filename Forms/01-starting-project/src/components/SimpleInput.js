207bb75abf02585de70d588e1cb6961c7529425c
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-23 18:28:52 +0530

    basic form submission

---
+++ Forms/01-starting-project/src/components/SimpleInput.js
@@ -0,0 +1,31 @@
+import { useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState(''); //per keystroke
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault(); //do nothing, don't send a POST request
+    const enteredValue = nameInputRef.current.value;
+    //nameInputRef.current.value = ''; //direct manipulation of the DOM, not good!
+    setEnteredName('');
+
+  };
+  return (
+  
+    <form onSubmit={formSubmissionHandler}>
+      <div className='form-control'>
+        <label htmlFor='name'>Your Name</label>
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
