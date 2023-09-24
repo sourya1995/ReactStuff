9c07599255cc54c621efe8f61eacefb805faed81
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-24 10:43:05 +0530

    adding useReducer to custom hook

--- Forms/01-starting-project/src/hooks/use-input.js
+++ Forms/01-starting-project/src/hooks/use-input.js
@@ -1,35 +1,49 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+const initialInputState = {
+    value: '',
+    isTouched: false
+};
+
+const inputStateReducer = (state, action) => {
+    if (action.type === 'INPUT') {
+        return { value: action.value, isTouched: state.isTouched };
+    }
+    if (action.type === 'BLUR') {
+        return { isTouched: true, value: state.value };
+    }
+    if (action.type === 'RESET') {
+        return { isTouched: false, value: ' ' };
+    }
+    return inputStateReducer;
+};
 
 const useInput = (validateValue) => {
-    const [enteredValue, setEnteredValue] = useState(''); //per keystroke
+    const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
+    const valueIsValid = validateValue(inputState.value);
+    const hasError = !valueIsValid && inputState.isTouched;
 
-    const [isTouched, setIsTouched] = useState(false);
+    const valueChangeHandler = (event) => {
+        dispatch({ type: 'INPUT', value: event.target.value });
 
-    const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+    };
 
-  const valueChangeHandler = event => {
-    setEnteredValue(event.target.value);
+    const inputBlurHandler = (event) => {
+        dispatch({ type: 'BLUR' });
+    }
 
-  };
+    const reset = () => {
+        dispatch({ type: 'RESET' });
+    }
 
-  const inputBlurHandler = event => {
-    setIsTouched(true);
-  }
-
-  const reset = () => {
-    setEnteredValue('');
-    setIsTouched(false);
-  }
-
-  return {
-    value: enteredValue,
-    isValid: valueIsValid,
-    hasError,
-    valueChangeHandler,
-    inputBlurHandler,
-    reset
-  }
+    return {
+        value: inputState.value,
+        isValid: valueIsValid,
+        hasError,
+        valueChangeHandler,
+        inputBlurHandler,
+        reset
+    }
 };
 
 
