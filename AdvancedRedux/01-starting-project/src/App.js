887885cac8b256e777e7d700b7ef31b1a04bb2df
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-26 18:29:11 +0530

    segregating side effect and data transformation with useEffect

--- AdvancedRedux/01-starting-project/src/App.js
+++ AdvancedRedux/01-starting-project/src/App.js
@@ -4,6 +4,13 @@ import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    fetch('https://react-http-cb45a-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   return (
     <Layout>
       <Cart />
