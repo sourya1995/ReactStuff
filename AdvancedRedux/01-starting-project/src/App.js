9ecd3825db4e7a9533069d9ff9282d04ea2fb129
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-26 19:26:55 +0530

    action creator thunks

--- AdvancedRedux/01-starting-project/src/App.js
+++ AdvancedRedux/01-starting-project/src/App.js
@@ -14,41 +14,12 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
   useEffect(() => {
-    const sendCartData = async () => {
-    dispatch(uiActions.showNotification({
-      status: 'pending',
-      title: 'Sending...',
-      message: 'Sending cart data...',
-    }));
-    const response = await fetch('https://react-http-cb45a-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
-    });
-
-    if(!response.ok){
-      throw new Error('Sending cart data failed');
+    if(isInitial){
+      isInitial = false;
+      return;
     }
-    dispatch(uiActions.showNotification({
-      status: 'success',
-      title: 'Success!',
-      message: 'Sent cart data successfully',
-    }));
 
-    
-
-  };
-
-  if(isInitial){
-    isInitial = false;
-    return;
-  }
-  sendCartData().catch(error => {
-    dispatch(uiActions.showNotification({
-      status: 'error',
-      title: 'Error',
-      message: 'Sending cart data failed',
-    }));
-  });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
 
