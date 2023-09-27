a8531ecb3e6e62fe627be1ca503c6437a81d2f22
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-27 11:02:25 +0530

    nested routes

--- Router/01-starting-project/src/App.js
+++ Router/01-starting-project/src/App.js
@@ -1,14 +1,17 @@
 import { createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/Root";
 
 
 const router = createBrowserRouter([
-  {path: '/', element: <HomePage />},
-  {path: '/products', element: <Products />},
+  {
+    path: '/', element: <RootLayout />, children: [{ path: '/', element: <HomePage /> },
+    { path: '/products', element: <Products /> },]
+  },
 
 ]);
 
 function App() {
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ Router/01-starting-project/src/components/MainNavigation.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import classes from './MainNavigation.module.css';
+
+function MainNavigation() {
+    return <header className={classes.header}>
+        <nav>
+            <ul className={classes.list}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/Products">Products</Link></li>
+            </ul>
+        </nav>
+    </header>
+
+}
+
+export default MainNavigation;
