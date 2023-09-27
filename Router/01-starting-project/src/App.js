fe74a0bc9b64bf081e797f5788a77a9466710662
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-27 11:07:09 +0530

    route error handling

--- Router/01-starting-project/src/App.js
+++ Router/01-starting-project/src/App.js
@@ -1,11 +1,12 @@
 import { createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 
 const router = createBrowserRouter([
   {
-    path: '/', element: <RootLayout />, children: [{ path: '/', element: <HomePage /> },
-    { path: '/products', element: <Products /> },]
+    path: '/', element: <RootLayout />, errorElement: <ErrorPage />, children: [{ path: '/', element: <HomePage /> },
+    { path: '/products', element: <Products />, },]
   },
 
 ]);
