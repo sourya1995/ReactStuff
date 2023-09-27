fd70532b963105bf2c710f81f52ef19b7b6a1eb1
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-27 10:09:13 +0530

    adding React Router

---
+++ Router/01-starting-project/src/App.js
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage />},
+  {}
+
+])
+
+function App() {
+  return <RouterProvider router={router}/>;
+}
+
+export default App;
