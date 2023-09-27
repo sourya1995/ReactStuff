6a4df8c35b274c84594a6b44d87852259e609674
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-27 11:16:44 +0530

    programmatic navigation

--- Router/01-starting-project/src/pages/Home.js
+++ Router/01-starting-project/src/pages/Home.js
@@ -1,10 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function HomePage() {
+    const navigate = useNavigate();
 
+    function navigateHandler(){
+        navigate('/products');
+    }
     return (
         <div>
             <h1>My Home Page!</h1>
-            <p>Go to <Link to="/products" /> the list of products</p> 
+            <p>Go to <Link to="/products" /> the list of products</p>
+            <p>
+                <button onClick={navigateHandler}>Navigate</button>
+            </p> 
         </div>
     );
 }
