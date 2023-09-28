5f58530bd16828f73c498e958f260cec5f889240
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-28 08:54:54 +0530

    adding automatic logout, clear token after an hour

--- Authentication/01-starting-project/frontend/src/pages/Root.js
+++ Authentication/01-starting-project/frontend/src/pages/Root.js
@@ -1,9 +1,19 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useNavigation } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    if(!token){
+      return;
+    }
+    setTimeout(() => {
+      submit(null, {action: '/logout', method: 'post'}, 1 * 60 * 60 * 1000);
+    }, timeout);
+  }, [token, submit]);
 
   return (
     <>
