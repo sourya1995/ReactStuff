e686e2783b9546335d7fb23edacdeed9888c2af3
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-28 12:27:04 +0530

    useMutation to send data

--- TanstackQuery/starting-project/src/components/Events/FindEventSection.jsx
+++ TanstackQuery/starting-project/src/components/Events/FindEventSection.jsx
@@ -1,10 +1,40 @@
 import { useRef } from 'react';
-
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvents } from '../../util/http';
+import { useState } from 'react';
+import LoadingIndicator from '../UI/LoadingIndicator';
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', {search: searchTerm}],
+    queryFn: ({ signal }) => fetchEvents({signal, searchTerm}),
+    enabled: searchTerm !== ''
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
+  }
+
+  let content = <p>Please enter a search term</p>;
+
+  if(isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if(isError) {
+    content = <ErrorBlock title = "An error has occurred" message = {error.info?.message || "Failed to fetch events."} />;
+  }
+
+  if(data){
+    content = <ul className='events-list'>
+      {data.map((event) => (
+        <li key={event.id}>
+          <EventItem event={event} />
+        </li>
+      ))}
+    </ul>
   }
 
   return (
@@ -20,6 +50,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
+      {content}
       <p>Please enter a search term and to find events.</p>
     </section>
   );
--- TanstackQuery/starting-project/src/components/Events/NewEvent.jsx
+++ TanstackQuery/starting-project/src/components/Events/NewEvent.jsx
@@ -1,16 +1,25 @@
 import { Link, useNavigate } from 'react-router-dom';
-
+import { useMutation } from '@tanstack/react-query';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { createNewEvent } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
   const navigate = useNavigate();
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent
+  });
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    mutate({event: formData});
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
+        {isPending && 'Submitting...'}
+        {!isPending && 
         <>
           <Link to="../" className="button-text">
             Cancel
@@ -19,7 +28,9 @@ export default function NewEvent() {
             Create
           </button>
         </>
+}
       </EventForm>
+      {isError && <ErrorBlock title="Failed to create event" message={error.info?.message || "Failed to create event"} />
     </Modal>
   );
 }
