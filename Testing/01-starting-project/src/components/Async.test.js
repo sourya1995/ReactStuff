b783b5ea8b6b07bce2b53afc5277246f7a9a7a33
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-29 17:01:01 +0530

    Adding mocks

--- Testing/01-starting-project/src/components/Async.test.js
+++ Testing/01-starting-project/src/components/Async.test.js
@@ -2,6 +2,10 @@ import Async from "./Async";
 
 describe('Async component', () => {
     test('renders posts if request succeeds', async () => {
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async() => [{id: 'p1', title: 'First Post'}, {id: 'p2', title: 'Second Post'}],
+        });
         render(<Async />);
         const listItemElements = await screen.findAllByRole('listitem'); //returns promise
         expect(listItemElements).not.toHaveLength(0);
