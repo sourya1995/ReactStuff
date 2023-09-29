326ed4e7d1750b5c2d2305fb4d8f297afa0abc4c
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-29 10:54:18 +0530

    using scrollable animation

--- FramerMotion/01-starting-project/src/components/Badge.jsx
+++ FramerMotion/01-starting-project/src/components/Badge.jsx
@@ -1,3 +1,3 @@
 export default function Badge({ caption }) {
-  return <span className="badge">{caption}</span>;
+  return <motion.span animate={{ scale: [1, 1.2, 1]}} transition={{ duration: 0.3 }} className="badge">{caption}</motion.span>;
 }
