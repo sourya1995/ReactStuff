5ebadbb931d49d70cc2ae4122cbdc8b6af8fc516
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-09-29 20:15:29 +0530

    form submission

---
+++ Typescript/react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,20 @@
+import { useRef } from "react";
+
+const NewTodo = () =>{
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        const enteredText = todoTextInputRef.current?.value
+        if(enteredText?.trim().length === 0){
+            return;
+        }
+    }
+    return <form onSubmit={submitHandler}>
+        <label>Todo text</label>
+        <input type="text" id="text" ref={todoTextInputRef}/>
+        <button>Add Todo</button>
+    </form>
+}
+
+export default NewTodo;
